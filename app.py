76a70e100a44418b36dba0e0f86285fc2569e891
swappnet <vladokuskov@gmail.com>
2024-10-01 19:58:59 +0300

    Added notifications item adding, back button, deletion of item

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 ALLOWED_USERS = [414510674]
+NOTIFICATIONS_LIMIT = 5
 
 async def check_authorization(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
@@ -26,9 +27,10 @@ def authorized(func):
     return wrapper
 
 # Menu
-def create_reply_keyboard():
+def main_reply_keyboard():
     keyboard = [
-        [KeyboardButton("/search")]
+        [KeyboardButton("/search")],
+        [KeyboardButton("/notifications")]
     ]
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
@@ -43,24 +45,77 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Sorry, you are not authorized to use this command.")
         return
 
-    reply_markup = create_reply_keyboard()
+    reply_markup = main_reply_keyboard()
 
     await update.message.reply_text('Welcome! Please choose an option:', reply_markup=reply_markup)
+
 
 @authorized
 async def search(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Please enter the item name you want to search for:')
-    context.user_data['waiting_for_item'] = True
+    context.user_data['waiting_for_search'] = True
+
+
+@authorized
+async def notifications(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    tracked_items = context.user_data.get('tracked_items', [])
+
+    # Initialize the keyboard with the "Back" button
+    keyboard = [[KeyboardButton("Back")]]
+
+    if not tracked_items:
+        # No items added, show "Add item to track" button
+        keyboard.insert(0, [KeyboardButton("Add item to track")])  # Insert at the top
+        reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        await update.message.reply_text('You have not added any items to track.', reply_markup=reply_markup)
+    else:
+        response = "You have the following items tracked:\n\n" + "\n".join(tracked_items)
+
+        # Add buttons for item removal
+        for item in tracked_items:
+            keyboard.insert(-1, [KeyboardButton(item)])  # Add item buttons before "Back"
+
+        if not len(tracked_items) + 1 > NOTIFICATIONS_LIMIT:
+            keyboard.insert(0, [KeyboardButton("Add item to track")])
+
+        reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        response += "\n\nUse the button below to go back."
+        await update.message.reply_text(response, reply_markup=reply_markup)
 
 
 # Input
 @authorized
 async def handle_item_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if context.user_data.get('waiting_for_item'):
+    # Check if the user is in the process of adding an item
+    if context.user_data.get('waiting_for_search'):
         item_name = update.message.text
         context.user_data['item_name'] = item_name
-        context.user_data['waiting_for_item'] = False
+        context.user_data['waiting_for_search'] = False
         await fetch_olx_data(update, context)
+    elif context.user_data.get('adding_item'):
+        # Add item to track
+        item_name = update.message.text
+        tracked_items = context.user_data.setdefault('tracked_items', [])
+        if item_name not in tracked_items:
+            tracked_items.append(item_name)
+            await update.message.reply_text(f'Item "{item_name}" has been added to your tracked items.')
+        else:
+            await update.message.reply_text(f'You already have "{item_name}" in your tracked items.')
+        await notifications(update, context)
+        context.user_data['adding_item'] = False  # Reset adding_item flag
+    elif update.message.text == "Add item to track":
+        # Set flag to indicate waiting for item name
+        context.user_data['adding_item'] = True
+        await update.message.reply_text("Type item name:")
+    elif update.message.text in context.user_data.get('tracked_items', []):
+        # Remove the selected item
+        item_to_remove = update.message.text
+        tracked_items = context.user_data['tracked_items']
+        tracked_items.remove(item_to_remove)
+        await update.message.reply_text(f'Item "{item_to_remove}" has been removed from your tracked items.')
+        await notifications(update, context)
+    elif update.message.text == "Back":
+        await update.message.reply_text('Main menu. Choose an option:', reply_markup=main_reply_keyboard())
     else:
         await update.message.reply_text("Please use /search to start a new search.")
 
@@ -122,6 +177,8 @@ def main() -> None:
     # Add handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("search", search))
+    application.add_handler(CommandHandler("notifications", notifications))
+
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_item_input))
 
     # Run the bot until the user presses Ctrl-C
